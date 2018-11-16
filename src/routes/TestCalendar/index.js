f1842da8eb0ec6d8de01535fccf43333ac562c47
weiyingHappy <949736522@qq.com>
2018-11-16 09:35:00 +0800

    test

--- src/routes/TestCalendar/index.js
+++ src/routes/TestCalendar/index.js
@@ -1,69 +1,132 @@
 
 import React, { Component } from 'react';
-import customBiz from './js/calendar_showcase.js'
+import Calendar from './libs/calendar_base.js'
+
 import './css/mui.min.css'
 import './libs/swiper.min.css'
 import './libs/calendar_base.css'
 import './css/showcase.css'
 
 class MobileCalendar extends Component {
-  componentDidMount() {
-    const el = document.getElementById('calendar');
-    if (el) {
-      customBiz.init(this.renderDayItem);
+  constructor(props) {
+    super(props);
+    this.customBiz = {
+      init: function (template) {
+        var self = this;
+        // 初始化日历
+        var calendar = new Calendar({
+          // swiper滑动容器
+          container: "#calendar",
+          // 上一月节点
+          pre: ".pre",
+          // 下一月节点
+          next: ".next",
+          template: template,
+          // 回到今天
+          backToToday: ".backToday",
+          // 业务数据改变
+          dataRequest: function (currdate, callback, _this) {
+            // 无日程安排
+            var data = [{
+              "date": "2018-04-18"
+            }, {
+              "date": "2018-04-17"
+            }, {
+              "date": "2018-04-16"
+            }];
+            callback && callback(data);
+          },
+          // 点击日期事件
+          onItemClick: function (item) {
+
+            var defaultDate = item.date;
+            // 设置标题
+            setTitle(defaultDate);
+          },
+          // 滑动回调
+          swipeCallback: function (item) {
+            var defaultDate = item.date;
+            // 设置标题
+            setTitle(defaultDate);
+            // 动态新增点击样式
+            calendar.addActiveStyleFordate(defaultDate);
+
+          },
+          // 调试
+          isDebug: false
+        });
+        // 设置标题
+        var titleNode = document.querySelector('.mid span');
+
+        function setTitle(date) {
+          titleNode.innerText = date;
+        }
+      }
     }
   }
 
-  renderDayItem = (value,curr)=> {
-    let template='';
+  componentDidMount() {
+    const el = document.getElementById('calendar');
+    if (el) {
+      this.customBiz.init(this.renderDayItem);
+    }
+  }
+
+  renderDayItem = (value, curr) => {
+
+    let template = '';
     if (value.date == curr) {
       //今天
       if (value.isSelected) {
-          template = '<div class="em-calendar-item em-calendar-active  isforbid{{isforbid}}" date="{{date}}"><span class="day">{{day}}</span><span class="dot dot-type1"></span></div>';
+        template = '<div class="em-calendar-item em-calendar-active  isforbid{{isforbid}}" date="{{date}}"><span class="day">{{day}}</span><span class="dot dot-type1"></span></div>';
       } else {
-          template = '<div class="em-calendar-item em-calendar-active  isforbid{{isforbid}}" date="{{date}}"><span class="day">{{day}}</span></div>';
+        template = '<div class="em-calendar-item em-calendar-active  isforbid{{isforbid}}" date="{{date}}"><span class="day">{{day}}</span></div>';
       }
-  } else {
+    } else {
       if (value.isSelected) {
-          // 个性化和业务贴近
-          template = '<div>"{{date}}"</div>';
+        // 个性化和业务贴近
+        template = '<div>"{{date}}"</div>';
       } else {
-        const style='color:red';
-        const test=1
-          template = `<div class='em-calendar-item  isforbid tip{{tip}}' date={{date}}><span class="day" test=${test} style=${style}>{{day}}</span></div>`;
+        const style = 'color:red';
+        const test = 1
+        template = `<div class='em-calendar-item  isforbid tip{{tip}}' date={{date}}><span class="day" test=${test} style=${style}>{{day}}</span></div>`;
       }
-  }
+    }
     return template
   }
   render() {
     return (
       <div>
-      <div className="mui-content">
-        <div className="em-journal-title">
-          <div className="em-per-block pre">
-            <span className="mui-icon mui-icon-arrowleft"></span>
-            <span>上一月</span>
+        <div className="mui-content">
+          <div className="em-journal-title">
+            <div className="em-per-block pre">
+              <span className="mui-icon mui-icon-arrowleft"></span>
+              <span>上一月</span>
+            </div>
+            <div className="em-per-block mid">
+              <span>...</span>
+            </div>
+            <div className="em-per-block next">
+              <span>下一月</span>
+              <span className="mui-icon mui-icon-arrowright"></span>
+            </div>
           </div>
-          <div className="em-per-block mid">
-            <span>...</span>
-          </div>
-          <div className="em-per-block next">
-            <span>下一月</span>
-            <span className="mui-icon mui-icon-arrowright"></span>
-          </div>
-        </div>
 
-        <div id="calendar">
+          <div id="calendar">
+
+          </div>
 
         </div>
-       
+        <div className="backToday">
+          返回今天
+    </div>
       </div>
-      <div className="backToday">
-			返回今天
-    </div>
-    </div>
     );
   }
 }
+MobileCalendar.defaultProps = {
+  template: () => {
 
+  }
+}
 export default MobileCalendar;
